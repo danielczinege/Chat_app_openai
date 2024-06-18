ecd070a1e9677f9fb5e6c94eaf7d681970d715f0
danielczinege <danczinege@gmail.com>
2024-06-18 20:46:43 +0200

    Now the message appears before response, not at the same time.

--- apps/chat-ui/src/pages/chat/chat.component.ts
+++ apps/chat-ui/src/pages/chat/chat.component.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
 import {ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 import { Message } from '@ukol-01/common';
 import { MessageComponent } from '../../components/message/message.component';
-import { Observable, Subject, Subscription, catchError, map, of, scan, switchMap, tap } from 'rxjs';
+import { Observable, Subject, Subscription, catchError, concatMap, map, of, scan, startWith, switchMap, tap } from 'rxjs';
 
 @Component({
     selector: 'app-chat',
@@ -19,22 +19,19 @@ export class ChatComponent {
         message: new FormControl('', {nonNullable: true}),
     });
     processing_response = false;
+
     message = new Subject<string>();
     messages$: Observable<Message[]> = this.message.asObservable().pipe(
         switchMap((current_message: string) => {
             return this.chatService.getResponse({text: current_message}).pipe(
-                map((response) => {
-                    return [{who: 'user', content: current_message},
-                            {who: 'AI', content: response.message}] as Message[]
-                }),
-                catchError((error: any, caught: Observable<any>) => {
-                return of([{who: 'user', content: current_message},
-                           {who: 'AI', content: "error occured"}] as Message[]);
-            }));
-        }), scan((acumulator, messages, index) => [...acumulator, ...messages], [] as Message[]),
-         tap(() => {
-            this.processing_response = false;
-        })
+                map((response) => ({who: 'AI', content: response.message} as Message)),
+                catchError((error: any, caught: Observable<any>) => of({who: 'AI', content: "error occurred"} as Message)),
+                startWith({who: 'user', content: current_message} as Message),
+                tap(() => {
+                    this.processing_response = false;
+                }));
+        }),
+        scan((acumulator, message, index) => [...acumulator, message], [] as Message[])
     );
 
     constructor(private chatService: ChatService) {}
