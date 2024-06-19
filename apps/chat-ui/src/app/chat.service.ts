3454aebffb4e8b603e1a255b62f7412cfdffea16
danielczinege <danczinege@gmail.com>
2024-06-19 11:59:58 +0200

    The messages does not dissapear when clicked on home and then back.

--- apps/chat-ui/src/app/chat.service.ts
+++ apps/chat-ui/src/app/chat.service.ts
@@ -1,12 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { IResponse, MessageRequest } from '@ukol-01/common';
-import { Observable } from 'rxjs';
+import { IResponse, Message, MessageRequest } from '@ukol-01/common';
+import { Observable, Subject, catchError, map, of, scan, shareReplay, startWith, switchMap } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ChatService {
+    processing_response = false;
+    message = new Subject<string>();
+    messages$: Observable<Message[]> = this.message.asObservable().pipe(
+        switchMap((current_message: string) => {
+            return this.getResponse({text: current_message}).pipe(
+                map((response) => {
+                    this.processing_response = false;
+                    return ({who: 'AI', content: response.message} as Message)
+                }),
+                catchError((error: any, caught: Observable<any>) => {
+                    this.processing_response = false;
+                    return of({who: 'AI', content: "error occurred"} as Message)
+                }),
+                startWith({who: 'user', content: current_message} as Message));
+        }),
+        scan((acumulator, message) => [...acumulator, message], [] as Message[]),
+        shareReplay(1)
+    );
+
     constructor(private http: HttpClient) {}
 
     getResponse(message: MessageRequest) {
--- apps/chat-ui/src/pages/chat/chat.component.ts
+++ apps/chat-ui/src/pages/chat/chat.component.ts
@@ -1,10 +1,8 @@
 import { ChatService } from './../../app/chat.service';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
-import { Message } from '@ukol-01/common';
 import { MessageComponent } from '../../components/message/message.component';
-import { Observable, Subject, catchError, map, of, scan, startWith, switchMap, tap } from 'rxjs';
 
 @Component({
     selector: 'app-chat',
@@ -13,30 +11,12 @@ import { Observable, Subject, catchError, map, of, scan, startWith, switchMap, t
     templateUrl: './chat.component.html',
     styleUrl: './chat.component.css',
 })
-export class ChatComponent {
+export class ChatComponent implements OnDestroy {
     messageForm = new FormGroup({
         message: new FormControl('', {nonNullable: true}),
     });
-    processing_response = false;
 
-    message = new Subject<string>();
-    messages$: Observable<Message[]> = this.message.asObservable().pipe(
-        switchMap((current_message: string) => {
-            return this.chatService.getResponse({text: current_message}).pipe(
-                map((response) => {
-                    this.processing_response = false;
-                    return ({who: 'AI', content: response.message} as Message)
-                }),
-                catchError((error: any, caught: Observable<any>) => {
-                    this.processing_response = false;
-                    return of({who: 'AI', content: "error occurred"} as Message)
-                }),
-                startWith({who: 'user', content: current_message} as Message));
-        }),
-        scan((acumulator, message) => [...acumulator, message], [] as Message[])
-    );
-
-    constructor(private chatService: ChatService) {}
+    constructor(public chatService: ChatService) {}
 
     handleSubmit() {
         let current_message = this.messageForm.value.message;
@@ -45,8 +25,12 @@ export class ChatComponent {
             return;
         }
 
-        this.processing_response = true;
+        this.chatService.processing_response = true;
         this.messageForm.reset();
-        this.message.next(current_message);
+        this.chatService.message.next(current_message);
+    }
+
+    ngOnDestroy(): void {
+        this.chatService.processing_response = false;
     }
 }
